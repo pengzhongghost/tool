6742f72836aca0c366263ef98f5a50829fa1d042
pengzhong <189968924@qq.com>
2024-04-11 15:23:10 +0800

    tool

--- src/main/java/tool/CsvTest.java
+++ src/main/java/tool/CsvTest.java
@@ -1,12 +1,10 @@
 package tool;
 
-import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.text.csv.CsvData;
 import cn.hutool.core.text.csv.CsvReader;
-import cn.hutool.core.text.csv.CsvRow;
 import cn.hutool.core.text.csv.CsvUtil;
 
-import java.util.List;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 /**
  * @author pengzhong
@@ -14,19 +12,12 @@ import java.util.List;
  */
 public class CsvTest {
 
-    public static void main(String[] args) {
-        CsvReader reader = CsvUtil.getReader();
-        CsvData data = reader.read(FileUtil.file("/Users/pengzhong/Downloads/redu_product__d40e0f16_1b64_4eb7_b21a_902db61ee9a0"));
-        List<CsvRow> rows = data.getRows();
-        for (CsvRow row : rows) {
-            for (String s : row.getRawList()) {
-                if (s.contains("男女皆可")) {
-                    System.out.println(s);
-                }
-                System.out.printf("%s\t", s);
-            }
+    public static void main(String[] args) throws FileNotFoundException {
+        CsvReader reader  = CsvUtil.getReader(new FileReader("/Users/pengzhong/Downloads/原始团长6月份.csv"));
+        reader.stream().forEach(row -> {
+            System.out.println(row.getRawList());
             System.out.println();
-        }
+        });
     }
 
 }
